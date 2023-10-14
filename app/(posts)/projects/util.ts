3fc687a51186a4644465f7ea1e067a2b5b27d18a
khoabot <khoadl@gmail.com>
2023-10-14 16:28:27 -0700

    Resolving type error

--- app/(posts)/projects/util.ts
+++ app/(posts)/projects/util.ts
@@ -31,40 +31,37 @@ async function getReadmeImage(repoName: string, owner: string): Promise<string |
 }
 
 export async function getAllProjects(page = 1): Promise<Post[]> {
-    const response = await fetch(
-        `https://api.github.com/users/tell-a-prompt/repos?sort=updated&per_page=100&direction=desc&page=${page}`
-    );
-  
-    const data = await response.json();
-  
-    const filteredData: Repo[] =
-        data.filter?.(
-            (repo: Repo) => !repo.fork && repo.description
-        ) ?? [];
-  
-    const projects = await Promise.all(
-        filteredData.map(async (repo) => {
-            const coverImage = await getReadmeImage(repo.name, repo.owner.login);
-            const views = await getRepoViews(repo.name, repo.owner.login);
-  
-            return {
-                slug: repo.name,
-                title: repo.name.replace(/[-_]/g, " "),
-                brief: repo.description,
-                date: repo.pushed_at,
-                url: repo.homepage ?? "",
-                content: "",
-                coverImage: coverImage ?? repo.owner.avatar_url,
-                type: "projects",
-                views,  // use the fetched view count
-                owner: repo.owner.login,
-            };
-        })
-    );
-  
-    return projects;
-  }
-  
+  const response = await fetch(
+    `https://api.github.com/users/tell-a-prompt/repos?sort=updated&per_page=100&direction=desc&page=${page}`
+  );
+
+  const data = await response.json();
+
+  const filteredData: Repo[] =
+    data.filter?.((repo: Repo) => !repo.fork && repo.description) ?? [];
+
+    const projects = (await Promise.all(
+      filteredData.map(async (repo) => {
+        const coverImage = await getReadmeImage(repo.name, repo.owner.login);
+        const views = await getRepoViews(repo.name, repo.owner.login);
+    
+        return {
+          slug: repo.name,
+          title: repo.name.replace(/[-_]/g, " "),
+          brief: repo.description,
+          date: repo.pushed_at,
+          url: repo.homepage ?? "",
+          content: "",
+          coverImage: coverImage ?? repo.owner.avatar_url,
+          type: "projects",
+          views,
+          owner: repo.owner.login,
+        };
+      })
+    )) as Post[];    
+
+  return projects;
+} 
 
 async function getRepoViews(repoName: string, owner: string): Promise<number> {
     const key = `${repoName}-views`;
