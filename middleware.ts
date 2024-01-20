c5946d46aace4a6a3a00e3320529fd9d98b31ae0
Khoa <khoadl@gmail.com>
2024-01-19 16:13:51 -0800

    Missed these ones...

--- middleware.ts
+++ middleware.ts
@@ -2,23 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const { nextUrl } = request;
-  const slug = nextUrl.pathname.split("/").pop();
   const response = NextResponse.next();
-
-  // ensure we track one view per request
-  if (!request.cookies.get(`${slug}-viewed`)) {
-    const currentViews = (await kv.get<number>(`${slug}-views`)) || 0;
-    await kv.set(`${slug}-views`, currentViews + 1);
-    response.cookies.set(`${slug}-viewed`, "1", {
-      maxAge: 60 * 60 * 24 * 365, // 1 year
-      path: "/",
-    });
-  }
 
   return response;
 }
-
-export const config = {
-  matcher: ["/blog/:slug*"],
-};
